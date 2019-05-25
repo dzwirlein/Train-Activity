e5513e80a054dd2d6609ead8f89027226cd818cb
dzwirlein <47755855+dzwirlein@users.noreply.github.com>
2019-05-25 15:30:47 -0500

    Functioning app

--- Assets/app.js
+++ Assets/app.js
@@ -35,8 +35,68 @@
 
         }
     database.ref().push(newTrain);
-  
-  
-})
 
 
+
+    $("#trainName").val("");
+    $('#destination').val("");
+    $('#trainTime').val("");
+    $('#frequency').val("");
+  
+  
+});
+
+database.ref().on('child_added', function(childSnapshot){
+
+    console.log(childSnapshot);
+
+        var trainName = childSnapshot.val().name;
+        var trainDestination = childSnapshot.val().destination;
+        var trainFrequency = childSnapshot.val().frequency;
+        var trainTime = moment(childSnapshot.val().firstTrain, 'HH:mm').format('hh:mm a');
+        
+
+
+        console.log(trainName);
+        console.log(trainDestination);
+        console.log(trainFrequency);
+        console.log(trainTime);
+
+
+
+
+    var trainTimeConverted = moment(trainTime, "hh:mm a").subtract(1, "day");
+    var currentTime = moment().format('hh:mm a');
+    var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+    var tRemainder = diffTime % trainFrequency;
+    var minAway = trainFrequency - tRemainder;
+    var nextTrain= moment().add(minAway, "minutes");
+    var nextArrival = moment(nextTrain).format("hh:mm a");
+
+
+    console.log(trainTimeConverted);
+    console.log(currentTime);
+    console.log(diffTime);
+    console.log(tRemainder);
+    console.log(minAway);
+    console.log(nextTrain);
+    console.log(nextArrival);
+
+      
+
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(trainDestination),
+            $("<td>").text(trainFrequency),
+            $("<td>").text(nextArrival),
+            $("<td>").text(minAway),
+        
+          );
+        
+          // Append the new row to the table
+          $("#schedule-table > tbody").append(newRow);
+
+
+        });
+
+    
